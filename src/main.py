d01eaf9c616c1b295d3863f4b6aebf258670e734
chinzhening <ning.chin03@gmail.com>
2024-04-16 00:54:14 +0800

    Integrate color and output functionality to main

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
-import logging
+from color import *
+from output import *
+from vec3d import *
 
-logger = logging.getLogger(__name__)
-
-def create_logger():
-    logging.basicConfig(filename='main.log', level=logging.INFO)
+log = Output('log')
+stdout = Output('stdout')
 
 if __name__ == '__main__':
-
-    create_logger()
-    logger.info('Started')
 
     # Image
     image_width : int = 256
@@ -20,19 +17,9 @@ if __name__ == '__main__':
     print('255')
     
     for j in range(image_height):
-        logger.info(f'\rScanlines remaining: {image_height - j}')
+        log.write(f'\rScanlines remaining: {image_height - j}')
         for i in range(image_width):
-            # Values from 0.0 to 1.0
-            r = i / (image_width - 1)
-            g = j / (image_height - 1)
-            b = 0.0
+            pixel_color = Color(np.array([i/(image_width-1), j/(image_height-1), 0]))
+            write_color(stdout, pixel_color)
 
-            # Scaled to 0 to 255
-            ir = int(255.999 * r)
-            ig = int(255.999 * g)
-            ib = int(255.999 * b)
-
-            print(f'{ir} {ig} {ib}')
-    
-
-    logger.info('Finished')
+    log.write('\rDone.')

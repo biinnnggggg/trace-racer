2f5d91dc8bf9ec0581f953d4ba082bb9fbdf36f4
chinzhening <ning.chin03@gmail.com>
2024-04-17 09:07:18 +0800

    Implement normal face tracking to hit record

--- src/hittable.py
+++ src/hittable.py
@@ -1,28 +1,69 @@
-import numpy as np
-
-from .ray import Ray
-from .vec3d import Vec3D, Point3D
+from .tracer_utility import *
 
 class HitRecord:
-    def __init__(self, p : Point3D=None, norm : Vec3D=None, t : float=None):
+    def __init__(self,
+                 p : Point3D=None,
+                 normal : Vec3D=None,
+                 t : float=None,
+                 front_face : bool = None) -> None:
         self.p = p
-        self.norm = norm
+        self.normal = normal
         self.t = t
+        self.front_face = front_face
+
+    def set_face_normal(self, r : Ray, outward_normal : Vec3D) -> None:
+        """Sets the hit record normal vector. Note that the parameter `outward_normal`
+        is assumed to have unit length.
+        """
+        assert np.isclose(outward_normal.length(), 1.0)
+        self.front_face = r.get_direction().dot(outward_normal) < 0
+        self.normal = outward_normal if self.front_face else -outward_normal
 
 class Hittable:
     """An abstract class all 'hittable' objects inherit from
     """
-    def hit(self, ray : Ray, t_min : float, t_max : float, hit_record : HitRecord) -> bool:
+    def hit(self, r : Ray, t_min : float, t_max : float, hit_record : HitRecord) -> bool:
         raise NotImplementedError
+    
+class HittableList(Hittable):
+    def __init__(self):
+        self.obs = []
+    
+    def add(self, hob : Hittable) -> None:
+        self.obs.add(hob)
+    
+    def hit(self,
+            r : Ray,
+            t_min : float,
+            t_max : float,
+            rec : HitRecord) -> bool:
+        temp_rec : HitRecord = HitRecord()
+        hit_any : bool
+        nearest = t_max
+
+        for hob in self.obs:
+            if hob.hit(r, t_min, nearest, temp_rec):
+                hit_any = True
+                nearest = temp_rec.t
+                
+                rec.p = temp_rec.p
+                rec.normal = temp_rec.normal
+                rec.t = temp_rec.t
+                rec.front_face = temp_rec.front_face
+        
+        return hit_any
+
+
+# Geometries
 
 class Sphere(Hittable):
-    def __init__(self, center : Point3D, radius : float):
+    def __init__(self, center : Point3D, radius : float) -> None:
         self.__center = center
         self.__radius = radius
 
-    def hit(self, ray : Ray, t_min : float, t_max : float, record : HitRecord) -> bool:
-        q = ray.get_origin()
-        d = ray.get_direction()
+    def hit(self, r : Ray, t_min : float, t_max : float, rec : HitRecord) -> bool:
+        q = r.get_origin()
+        d = r.get_direction()
 
         a = d.dot(d)
         h = d.dot(self.__center - q)
@@ -50,8 +91,9 @@ class Sphere(Hittable):
         if t is None:
             return False
         
-        record.p = ray.at(t)
-        record.norm = (record.p - self.__center) / self.__radius
-        record.t = t
+        rec.t = t
+        rec.p = r.at(t)
+        outward_normal = (rec.p - self.__center) / self.__radius
+        rec.set_face_normal(r, outward_normal)        
 
         return True
